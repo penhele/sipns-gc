4c845999c21ba521f91676dc5a96b4d8aaaea3e9
Stephen Helenus <stephenhelk@gmail.com>
2026-06-02 13:52:31 +0700

    feat: create login page

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,5 @@
+import LoginPage from "@/features/auth/login/components/login-page";
+
+export default function Page() {
+  return <LoginPage />;
+}
---
+++ features/auth/login/components/login-page.tsx
@@ -0,0 +1,12 @@
+export default function LoginPage() {
+  return (
+    <div className="grid grid-cols-2 h-screen bg-muted">
+      <div className="">
+        
+      </div>
+      <div className="">
+        hi
+      </div>
+    </div>
+  );
+}
