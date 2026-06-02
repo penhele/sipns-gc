88e0fb93b14dffc3157f3c871e2f56d2046a7b64
Stephen Helenus <stephenhelk@gmail.com>
2026-06-02 18:59:22 +0700

    feat: add sidebar

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,23 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from "@/components/ui/sidebar";
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup>
+            <SidebarContent>
+                
+            </SidebarContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
+}
